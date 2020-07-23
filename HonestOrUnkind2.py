67847ec8925aea3188f88e412c29fef794a91e77
KoichiHirai <i.remember_tt@icloud.com>
2020-07-23 22:00:16 +0900

    2020/7/23 WA

---
+++ HonestOrUnkind2.py
@@ -0,0 +1,36 @@
+# https://atcoder.jp/contests/abc147/tasks/abc147_c
+
+n = int(input())
+
+list_a = []
+testimonies = []
+answer = 0
+flag = True
+
+for i in range(n):
+	testimony = []
+	a = int(input())
+	list_a.append(a)
+	for j in range(a):
+		testimony.append(list(map(int,input().split())))
+	testimonies.append(testimony)
+
+# print(list_a)
+# print(testimonies)
+
+for i in range(1 << n): # 部分和の計算　i=2は"0b10" 
+	result = 0
+	for j in range(n): # 各桁を確認 jは桁数を表す
+		if i & (1 << (j)): #trueだった場合はj桁目は1である(後ろからj番目は正直者)
+			for k in range(list_a[-j]):
+				if i & testimonies[-j][k][1] << (testimonies[-j][k][0]-1):
+					flag = False
+					break
+			if flag == True:
+				result += 1
+			else:
+				flag = True
+	if answer < result:
+		answer = result
+
+print(answer)
