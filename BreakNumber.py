034a30425ae42a7713316d660eb3cb0232fbb114
KoichiHirai <i.remember_tt@icloud.com>
2020-07-31 20:03:27 +0900

    2020/7/31 AC

--- BreakNumber.py
+++ BreakNumber.py
@@ -2,6 +2,14 @@
 
 n = int(input())
 result = 0
-while(2 ** result < n):
-	result += 1
-print(2 ** result)
+if n == 1:
+	print(1)
+else:
+	while(True):
+		if 2 ** result == n:
+			break
+		if 2 ** result > n:
+			result -= 1
+			break
+		result += 1
+	print(2 ** result)
