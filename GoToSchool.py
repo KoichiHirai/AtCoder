0f26740adb89db282f1f7a1d8d4cc6c003a3d39e
KoichiHirai <i.remember_tt@icloud.com>
2020-08-10 22:47:32 +0900

    2020/8/10 AC

--- GoToSchool.py
+++ GoToSchool.py
@@ -2,7 +2,10 @@
 
 n = int(input())
 a = list(map(int,input().split()))
-answer = []
+answer = [0] * n
 
-for i in range(1,n+1):
-	print(a.index(i)+1, end = " ")
+for i in range(n):
+	answer[a[i]-1] = i + 1 
+
+for i in range(n):
+	print(answer[i], end = " ")
