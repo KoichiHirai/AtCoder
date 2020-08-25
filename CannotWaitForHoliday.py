5cf879a8e757427eeb0b8650cb500dbf84434378
KoichiHirai <i.remember_tt@icloud.com>
2020-08-25 23:31:57 +0900

    2020/8/25 AC

---
+++ CannotWaitForHoliday.py
@@ -0,0 +1,18 @@
+# https://atcoder.jp/contests/abc146/tasks/abc146_a
+
+s = input()
+
+if s == "SUN":
+	print(7)
+elif s == "MON":
+	print(6)
+elif s == "TUE":
+	print(5)
+elif s == "WED":
+	print(4)
+elif s == "THU":
+	print(3)
+elif s == "FRI":
+	print(2)
+else:
+	print(1)
