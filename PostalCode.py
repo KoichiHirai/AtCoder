8ca6bc0076a1047d1a9e298bc12baf52a823f666
KoichiHirai <i.remember_tt@icloud.com>
2020-08-30 22:07:52 +0900

    2020/8/30 AC

---
+++ PostalCode.py
@@ -0,0 +1,6 @@
+# https://atcoder.jp/contests/abc084/tasks/abc084_b
+
+a,b = map(int,input().split())
+s = input()
+
+print("Yes") if s[0:a].isdigit() and  s[a] == "-" and s[a+1:a+b+1].isdigit() else print("No")
