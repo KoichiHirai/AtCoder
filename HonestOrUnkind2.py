9611564e6c1f16dce539cea92ea92f7ad694bc92
KoichiHirai <i.remember_tt@icloud.com>
2020-09-06 16:50:36 +0900

    2020/9/6

--- HonestOrUnkind2.py
+++ HonestOrUnkind2.py
@@ -1,4 +1,5 @@
-# https://atcoder.jp/contests/abc147/tasks/abc147_c
+# https://atcoder.jp/contests/abc147/tasks/
+# 入力を一旦回答に寄せて考えたほうがいい
 
 n = int(input())
 
@@ -15,19 +16,37 @@ for i in range(n):
 		testimony.append(list(map(int,input().split())))
 	testimonies.append(testimony)
 
-for i in range(1 << n): # 部分和の計算　i=2は"0b10" 
+for i in range(1 << n): # 部分和の計算　i=2は"0b10"  正直者の組み合わせの全パターン
 	result = 0
-	for j in range(n): # 各桁を確認 jは桁数を表す
-		if i & (1 << (j)): #trueだった場合はj桁目は1である(後ろからj番目は正直者)
-			for k in range(list_a[-j]):
-				if i & testimonies[-j][k][1] << (testimonies[-j][k][0]-1):
+	for j in range(n): # 各正直者の組み合わせを確認
+		if i & (1 << (j)): #trueだった場合はj番目は正直者
+			result += 1
+			for k in range(list_a[j]): #正直者の証言を確認
+				if not i ^ (testimonies[j][k][1] << testimonies[j][k][0]-1): # 虚偽の証言をしている時
 					flag = False
 					break
-			if flag == True:
-				result += 1
-			else:
-				flag = True
-	if answer < result:
-		answer = result
+			# if flag == False:
+			# 	break
+	if flag == True:
+		if answer < result:
+			answer = result
+	else:
+		flag = True
+	
+
+# for i in range(1 << (n-1)): # 部分和の計算　i=2は"0b10" 
+# 	result = 0
+# 	for j in range(n): # 各桁を確認 jは桁数を表す
+# 		if i & (1 << (j)): #trueだった場合はj桁目は1である(後ろからj番目は正直者)
+# 			for k in range(list_a[-j]):
+# 				if i & testimonies[-j][k][1] << (testimonies[-j][k][0]-1):
+# 					flag = False
+# 					break
+# 			if flag == True:
+# 				result += 1
+# 			else:
+# 				flag = True
+# 	if answer < result:
+# 		answer = result
 
 print(answer)
