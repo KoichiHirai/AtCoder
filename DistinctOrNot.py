6cbf9e5d4c1db13cb318d27038ce48114032eda0
KoichiHirai <i.remember_tt@icloud.com>
2020-10-24 11:48:26 +0900

    2020/10/24 AC

---
+++ DistinctOrNot.py
@@ -0,0 +1,9 @@
+# https://atcoder.jp/contests/abc154/tasks/abc154_c
+n = int(input())
+a = list(map(int,input().split()))
+a.sort()
+for i in range(n-1):
+	if a[i] == a[i+1]:
+		print("NO")
+		exit()
+print("YES")
---
+++ IMissYou.py
@@ -0,0 +1,2 @@
+# https://atcoder.jp/contests/abc154/tasks/abc154_b
+print("x" * len(input()))
