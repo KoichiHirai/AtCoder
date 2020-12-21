c3006d3bc6c8a81e3d26233bd11e97632b6faba6
KoichiHirai <i.remember_tt@icloud.com>
2020-12-21 23:36:05 +0900

    2020/12/21 AC

--- AcCepted.py
+++ AcCepted.py
@@ -1,29 +1,24 @@
 # https://atcoder.jp/contests/abc104/tasks/abc104_b
-s =  input()
-len_s = len(s)
-position_c = 0
-count_c = 0
+
+s = input()
 
 if s[0] != "A":
 	print("WA")
 	exit()
 
-for i in range(2,len_s-1):
-	if s[i] == "C":
-		count_c += 1
-		if count_c == 1:
-			position_c = i
-	if count_c != 1 :
-		print("WA")
-		exit()	
+position_C = s.find("C") 
+if position_C == -1 or position_C == 1 or position_C == len(s)-1:
+	print("WA")
+	exit()
 
-for i in range(len_s):
-	if i == 0 or i == position_c:
-		continue
-	else:
-		if s[i].isupper():
-			print(s[i])
-			print("WA")
-			exit()
+s_list = list(s)
+s_list.remove("A")
+s_list.remove("C") 
+
+
+for i in s_list:
+	if i.islower() == False:
+		print("WA")
+		exit()
 
 print("AC")
